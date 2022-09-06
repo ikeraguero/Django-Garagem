658a2cf1b52a18cc3653253b5bcdfc5b07c91e91
ikeraguero <ikerpires407@gmail.com>
2022-09-05 21:28:18 -0300

    update

--- garagem/settings.py
+++ garagem/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#wyj*3v0kltuf!l#^71y@)xid!)uxrkhtj5l8c%s0%+e9b@)%r'
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#wyj*3v0kltuf!l#^71y@)xid!)uxrkhtj5l8c%s0%+e9b@)%r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
